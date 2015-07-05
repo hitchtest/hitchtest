898a198978aca6e600bfe8842978f09f18a3e9a3
Colm O'Connor <colm.oconnor.github@gmail.com>
2015-07-05 23:16:39 +0800

    FEATURE : Tools to download and build packages for use with tests.

--- hitchtest/package.py
+++ hitchtest/package.py
@@ -1,19 +1,5 @@
-
-class HitchPackage(object):
-    def __init__(self):
-        pass
-
-    def _download(self, name, url):
-        pass
-
-    def _extract_archive(self, filename, destination):
-        pass
-
-    def build(self):
-        pass
-
-    def verify(self):
-        pass
+from os import path, makedirs
+import sys
 
 class PackageNotBuilt(Exception):
     pass
@@ -23,3 +9,51 @@ class PackageVerificationFailure(Exception):
 
 class InvalidPackageVersion(Exception):
     pass
+
+
+class HitchPackage(object):
+    name = None
+
+    def __init__(self):
+
+        # 3 directories above virtualenv python binary is the hitchdir
+        self.hitch_dir = path.abspath(
+            path.join(path.dirname(sys.executable), "..", "..")
+        )
+
+        if self.name is None:
+            raise PackageVerificationFailure("This package does not have self.name set!")
+
+    def get_build_directory(self):
+        build_directory = path.join(self.hitch_dir, "build")
+        if not path.exists(build_directory):
+            makedirs(build_directory)
+        return build_directory
+
+    def get_downloads_directory(self):
+        downloads_directory = path.join(self.hitch_dir, "downloads")
+        if not path.exists(downloads_directory):
+            makedirs(downloads_directory)
+        return downloads_directory
+
+    def check_version(self, version, versions_list, issues_url, name=None):
+        package_name = self.name if name is None else name
+        if version not in versions_list:
+            raise InvalidPackageVersion(
+                "{} version {} not in list of approved versions: \n{}\n"
+                "Raise a ticket at {} "
+                "if you think it should be.".format(package_name, version, versions_list, issues_url)
+            )
+        return version
+
+    def download_file(self, name, url):
+        pass
+
+    def extract_archive(self, filename, destination):
+        pass
+
+    def build(self):
+        pass
+
+    def verify(self):
+        pass
--- hitchtest/utils.py
+++ hitchtest/utils.py
@@ -1,9 +1,12 @@
 from IPython.terminal.embed import InteractiveShellEmbed
 import IPython
+import patoolib
+import requests
 import inspect
 import signal
-import sys
+import shutil
 import os
+import sys
 
 def to_underscore_style(text):
     """Changes "Something like this" to "something_like_this"."""
@@ -62,3 +65,48 @@ def ignore_signals():
     signal.signal(signal.SIGTERM, signal.SIG_IGN)
     signal.signal(signal.SIGHUP, signal.SIG_IGN)
     signal.signal(signal.SIGQUIT, signal.SIG_IGN)
+
+def extract_archive(filename, directory):
+    patoolib.extract_archive(filename, outdir=directory)
+
+
+def download_file(downloaded_file_name, url):
+    TOTAL_BLOCKS = 79
+
+    downloaded_file_name_temp = "{}.part".format(downloaded_file_name)
+
+    if os.path.exists(downloaded_file_name_temp):
+        os.remove(downloaded_file_name_temp)
+
+    if not os.path.exists(downloaded_file_name):
+        with open(downloaded_file_name_temp, "wb") as downloaded_file_handle:
+            log("Downloading {}\n".format(url))
+            response = requests.get(url, stream=True)
+            total_length = response.headers.get('content-length')
+            previous_done = None
+
+            if total_length is None: # no content length header
+                downloaded_file_handle.write(response.content)
+            else:
+                downloaded_bytes = 0
+                total_length = int(total_length)
+                for data in response.iter_content():
+                    downloaded_bytes += len(data)
+                    downloaded_file_handle.write(data)
+                    blocks_done = int(TOTAL_BLOCKS * float(downloaded_bytes) / float(total_length))
+                    percentage_done = "{:.1f}".format(
+                        100 * float(downloaded_bytes) / float(total_length)
+                    )
+                    if percentage_done != previous_done:
+                        previous_done = percentage_done
+                        log("\r{}% [{}{}]".format(
+                            percentage_done,
+                            '=' * blocks_done,
+                            ' ' * (TOTAL_BLOCKS - blocks_done))
+                        )
+
+        log("\n")
+        shutil.move(downloaded_file_name_temp, downloaded_file_name)
+    else:
+        log("Downloaded : {} already found\n".format(downloaded_file_name))
+
