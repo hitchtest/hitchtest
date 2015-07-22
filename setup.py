5091efcd024c13d59140acb6d46609930245b002
Colm O'Connor <colm.oconnor.github@gmail.com>
2015-07-22 21:02:22 +0800

    RELEASE : Bumped version.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*parts):
     return codecs.open(os.path.join(os.path.abspath(os.path.dirname(__file__)), *parts), 'r').read()
 
 setup(name="hitchtest",
-      version="0.6.2",
+      version="0.6.3",
       description="Declarative test runner using YAML and jinja2.",
       long_description=read('README.rst'),
       classifiers=[
