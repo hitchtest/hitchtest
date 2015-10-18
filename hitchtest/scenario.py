206e54b9f93333e70c2177a922f65324e8577ce3
Colm O'Connor <colm.oconnor.github@gmail.com>
2015-10-18 14:38:41 +0800

    BUG : Handle empty scenarios by making an empty list of steps.

--- hitchtest/scenario.py
+++ hitchtest/scenario.py
@@ -1,11 +1,14 @@
 from hitchtest.step import Step
 
 class Scenario(object):
+    """The series of steps that make up a hitch test."""
+
     def __init__(self, yaml_scenario):
         self.steps = []
 
-        for i, yaml_step in enumerate(yaml_scenario, 1):
-            self.steps.append(Step(yaml_step, i))
+        if yaml_scenario is not None:
+            for i, yaml_step in enumerate(yaml_scenario, 1):
+                self.steps.append(Step(yaml_step, i))
 
     def to_dict(self):
         return [step.to_dict() for step in self.steps]
