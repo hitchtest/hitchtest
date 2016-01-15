c7487cdcdb86153cd2c13de7a28e54f409e423f6
Colm O'Connor <colm.oconnor.github@gmail.com>
2016-01-15 23:25:11 +0000

    FEATURE : Turned is_modified into a context manager that can be used with with.

--- hitchtest/modified.py
+++ hitchtest/modified.py
@@ -2,42 +2,54 @@ from os import path, makedirs
 from hitchtest.utils import get_hitch_directory
 import json
 
-
-def is_modified(includes, excludes=None):
-    """Return True if matching file(s) are either modified or no record of them being modified is kept."""
-
-    # If checking just for one file, make it a list
-    if type(includes) is str:
-        includes = [includes, ]
-
-    if excludes is None:
-        excludes = []
-
-    modififications_filename = path.join(get_hitch_directory(), "modified.json")
+class is_modified(object):
+    def __init__(self, includes, excludes=None):
+        self.includes = [includes, ] if type(includes) is str else includes
+        self.excludes = excludes if excludes is not None else []
+        self.modififications_filename = path.join(get_hitch_directory(), "modified.json")
     
-    if path.exists(modififications_filename):
-        with open(modififications_filename, 'r') as modifications_filename_handle:
-            modifications = json.loads(modifications_filename_handle.read())
-    else:
-        modifications = {}
-
-    at_least_one_modified = False
-
-    filenames = list(set(includes) - set(excludes))
-    
-    for relfilename in filenames:
-        absfilename = path.abspath(relfilename)
-        modified_time = path.getmtime(absfilename)
-        
-        if absfilename in modifications:
-            if modified_time > modifications[absfilename]:
-                modifications[absfilename] = modified_time
-                at_least_one_modified = True
+    def __enter__(self):
+        # Get all existing modification timestamps
+        if path.exists(self.modififications_filename):
+            with open(self.modififications_filename, 'r') as handle:
+                self.modifications = json.loads(handle.read())
         else:
-            modifications[absfilename] = modified_time
-            at_least_one_modified = True
+            self.modifications = {}
 
-    with open(modififications_filename, 'w') as modifications_file_handle:
-        modifications_file_handle.write(json.dumps(modifications))
+        at_least_one_modified = False
 
-    return at_least_one_modified
+        # Total list of filenames
+        filenames = list(set(self.includes) - set(self.excludes))
+
+        # Check if any of the files have been updated since
+        for relfilename in filenames:
+            absfilename = path.abspath(relfilename)
+            modified_time = path.getmtime(absfilename)
+            
+            if absfilename in self.modifications:
+                if modified_time > self.modifications[absfilename]:
+                    self.modifications[absfilename] = modified_time
+                    at_least_one_modified = True
+            else:
+                self.modifications[absfilename] = modified_time
+                at_least_one_modified = True
+
+        return at_least_one_modified
+
+    def __exit__(self, type, value, traceback):
+        # If an exception is thrown, don't update modifications
+        if traceback is None:
+            # Check for modifications by nested modification checkers
+            if path.exists(self.modififications_filename):
+                with open(self.modififications_filename, 'r') as handle:
+                    updated_modifications = json.loads(handle.read())
+            else:
+                updated_modifications = {}
+
+            # If a modification has been updated since __enter__, don't overwrite it
+            for filename in updated_modifications:
+                if updated_modifications[filename] > self.modifications[filename]:
+                    self.modifications[filename] = updated_modifications[filename]
+            
+            with open(self.modififications_filename, 'w') as handle:
+                handle.write(json.dumps(self.modifications))
