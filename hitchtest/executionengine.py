d463ddbc39336e250919184ab1587ff13c509b0e
Colm O'Connor <colm.oconnor.github@gmail.com>
2016-02-27 15:59:52 +0000

    FEATURE : Added self.path object that can be used instead of the more cumbersome PROJECT_DIRECTORY.

--- hitchtest/executionengine.py
+++ hitchtest/executionengine.py
@@ -2,15 +2,38 @@ import inspect
 import signal
 from hitchtest import utils
 from hitchtest.signal_manager import SignalManager
+from path import Path
+
 
 class HitchAbortedException(Exception):
     pass
+
+
+class HitchPathError(Exception):
+    pass
+
+
+class Paths(object):
+    def __init__(self):
+        self.hitch = Path(utils.get_hitch_directory())
+        self.engine = self.hitch.parent
+        self.state = self.hitch.joinpath("s")
+        
+        if not self.state.exists():
+            self.state.mkdir()
+        
+    def __setattr__(self, name, value):
+        if not isinstance(value, Path):
+            raise HitchPathError("Path '{}' must be of type pathlib.Path".format(value))
+        self.__dict__[name] = value
+
 
 class ExecutionEngine(object):
     def __init__(self, settings, preconditions):
         self.settings = settings
         self.preconditions = preconditions
         self.signal_manager = SignalManager(self.abort)
+        self.path = Paths()
 
     def set_up(self):
         pass
